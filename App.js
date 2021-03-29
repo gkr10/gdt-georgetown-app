aede351d8005691fc99e46593a3f90bd556fe969
Ralf Liu <ralphliu0921@gmail.com>
2021-03-29 06:46:28 -0400

    created and stylized header

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.hview}>
+      <Text style={styles.tview}>Georgetown University{"\n"}Farmer's Market</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  hview: {
+    backgroundColor: '#1B1513ff',
     justifyContent: 'center',
+    alignItems: 'center',
+    height: 110,
+    paddingTop: 30,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+    paddingHorizontal: 40,
   },
+  tview: {
+    fontSize: 24,
+    color: '#ffffff',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+
+  }
 });
