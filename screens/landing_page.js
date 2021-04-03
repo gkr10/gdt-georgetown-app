7fe0a498e8bd80777748fcdb3edc901779e97327
Ralf Liu <ralphliu0921@gmail.com>
2021-04-03 13:13:51 -0400

    storing app.js

--- screens/landing_page.js
+++ screens/landing_page.js
@@ -1,8 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Alert, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import * as React from 'react';
+import { Button, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-function landingpage() {
+function TodaysVendorsScreen() {
+  return (
+    <ScrollView style={styles.parent}>
+      <View style={styles.hview}>
+        <Text style={styles.tview}>Today's Vendors</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+function PopUpScreen() {
+  return (
+    <ScrollView style={styles.parent}>
+      <View style={styles.hview}>
+        <Text style={styles.tview}>Pop-Up (Seasonal)</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+function CommunityVendorsScreen() {
+  return (
+    <ScrollView style={styles.parent}>
+      <View style={styles.hview}>
+        <Text style={styles.tview}>Community Vendors</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+function AllVendorsScreen() {
+  return (
+    <ScrollView style={styles.parent}>
+      <View style={styles.hview}>
+        <Text style={styles.tview}>All Vendors</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Landing"
+        onPress={() => navigation.navigate('Landing')}
+      />
+    </View>
+  );
+}
+
+function LandingScreen({ navigation }) {
   return (
     <ScrollView style={styles.parent}>
       <View style={styles.hview}>
@@ -11,37 +64,54 @@ function landingpage() {
 
       <View style={styles.twobuttons}>
         <TouchableOpacity
-          onPress={console.log("Button Pressed")}
-          style={styles.gridbuttonL}>
+          style={styles.gridbutton}
+          onPress={() => navigation.push('TodaysVendors')}>
           <Text style={styles.buttontext}>Today's Vendors</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={console.log("Button Pressed")}
-          style={styles.gridbuttonR}>
+          style={styles.gridbutton}
+          onPress={() => navigation.push('PopUp')}>
           <Text style={styles.buttontext}>Pop-Up (Seasonal)</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.twobuttons}>
         <TouchableOpacity
-          onPress={console.log("Button Pressed")}
-          style={styles.gridbuttonL}>
+          style={styles.gridbutton}
+          onPress={() => navigation.push('CommunityVendors')}>
           <Text style={styles.buttontext}>Community Vendors</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={console.log("Button Pressed")}
-          style={styles.gridbuttonR}>
+          style={styles.gridbutton}
+          onPress={() => navigation.push('AllVendors')}>
           <Text style={styles.buttontext}>All Vendors</Text>
         </TouchableOpacity>
       </View>
       <TouchableOpacity
-        onPress={console.log("Button Pressed")}
         style={styles.todaylayout}>
           <Text style={styles.buttontext}>Today's Layout</Text>
       </TouchableOpacity>
-      
     </ScrollView>
   );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Landing" component={LandingScreen}/>
+        <Stack.Screen name="TodaysVendors" component={TodaysVendorsScreen}/>
+        <Stack.Screen name="PopUp" component={PopUpScreen}/>
+        <Stack.Screen name="CommunityVendors" component={CommunityVendorsScreen}/>
+        <Stack.Screen name="AllVendors" component={AllVendorsScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   todaylayout: {
@@ -58,25 +128,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
   },
-  gridbuttonL: {
-    marginTop: 50,
-    alignSelf: 'center',
-    marginRight: 20,
-    height: 140,
-    width: 190,
-    borderRadius: 35,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#725B45',
-  },
-  gridbuttonR: {
+  gridbutton: {
     marginTop: 50,
     alignSelf: 'center',
     height: 140,
     width: 190,
     borderRadius: 35,
     alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 10,
+    justifyContent: 'space-evenly',
     backgroundColor: '#725B45',
   },
   buttontext: {
@@ -90,7 +150,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     height: 110,
-    paddingTop: 30,
+    paddingVertical: 30,
     shadowColor: '#ffffff',
     shadowOffset: { width: 0, height: 10 },
     shadowOpacity: 0.2,
